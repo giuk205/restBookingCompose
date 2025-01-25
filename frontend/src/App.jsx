ece094cbbdb646efa7eeef0dbb95f0b8f1d37a4e
Giada <77614472+giuk205@users.noreply.github.com>
2025-01-25 10:17:36 +0100

    BookingCalenda provvisorio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+/*
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import User from "./pages/LivePortal";
+import LoginModal from "./components/LoginModal";
+import Navbar from "./components/Navbar";
+    <Router>
       <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact page={Home} />
+        <Route path="/User" exact page={User} />
+      </Routes>
+    </Router>
       <h1 className="text-3xl font-bold underline bg-amber-100 ">
         Hello Tailwind!!!
       </h1>
 
+      <div>
+      LoginModal /
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -32,6 +42,18 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      */
+import BookingCalendar from './components/BookingCalendar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <BookingCalendar maxGuests ={10} message = {"Questo è un messaggio"}/>
     </>
   )
 }
